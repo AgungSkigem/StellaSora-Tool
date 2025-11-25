4e953ca3da851c452a470fd48b54369f7705d933
AgungSkigem <113549339+AgungSkigem@users.noreply.github.com>
2025-11-25 16:41:03 +0700

    Update dllmain.cpp

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,10 +1,13 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
 #include "main.h"
+#include "debug.h"
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
 	switch (ul_reason_for_call) {
 		case DLL_PROCESS_ATTACH:
+			InitDebugConsole();
+    		DbgMsg("[StellaSora] DllMain: DLL_PROCESS_ATTACH");
 			CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Run, new HMODULE(hModule), 0, NULL);
 			break;
 		case DLL_THREAD_ATTACH:
